1263474fc34d36084c1b275722234b336059bf4f
Sonya <stesoi@mail.ru>
2019-06-08 11:43:03 +0300

    Solve problem with size of window

--- scan/scanner.py
+++ scan/scanner.py
@@ -1,10 +1,15 @@
 import cv2 as cv
 import numpy as np
 
+
 # параметры цветового фильтра
 lower_level = np.array([30, 150, 50])
 upper_level = np.array([255, 255, 180])
-WIDTH, HEIGHT = 100, 200
+# размеры экрана
+WIDTH, HEIGHT = 600, 600
+# толщина линии контура
+THICKNESS = 3
+
 
 def scanner(filename = ""):
     continue_cycle = True
@@ -21,12 +26,24 @@ def scanner(filename = ""):
         res = cv.bitwise_and(frame, frame, mask=mask) # вычисляет битовое соединение двух массивов или массива и скаляра
                                                       # для каждого элемента
 
-        cv.imshow('Original', frame) # отображение оригинального изображения
+        result = cv.Canny(frame, 100, 200)  # детектор границ Кенни
+
+        # ищем контуры и складируем их в переменную contours
+        contours, hierarchy = cv.findContours(result, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_TC89_KCOS)
+
+        # отображаем контуры поверх изображения
+        cv.drawContours(result, contours, -1, (255, 255, 255), THICKNESS, cv.LINE_AA, hierarchy, 1)
+
+        # # рисуем расположение долевой нити
+        # cv.arrowedLine(result, (20, 300), (20, 200), (250, 0, 0), 1)
+
+        # отображение полученных результатов
+        cv.namedWindow('Original', cv.WINDOW_NORMAL)
+        cv.imshow('Original', frame)
         cv.resizeWindow('Original', WIDTH, HEIGHT)
-
-        edges = cv.Canny(frame, 100, 200) # детектор границ Кенни
-
-        cv.imshow('Result', edges) # отображает полученное изображение
+        cv.namedWindow('Result', cv.WINDOW_NORMAL)
+        cv.imshow('Result', result)
+        cv.resizeWindow('Result', WIDTH, HEIGHT)
 
 
         k = (cv.waitKey(5) & 0xFF) if from_video else cv.waitKey(0) # выход по ESC
