677c8fee357a4dcf483b75c5fe05c0f81413db3d
Sonya <stesoi@mail.ru>
2019-07-09 15:58:34 +0300

    Add drop menu

--- mobile/mobile.py
+++ mobile/mobile.py
@@ -1,15 +1,28 @@
-import os
 from kivy.app import App
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.floatlayout import FloatLayout
+
 from kivymd.theming import ThemeManager
 from kivymd.list import MDList, OneLineListItem
+from kivymd.toast.kivytoast import toast
+
 
 class MenuPanel(AnchorLayout):
     def __init__(self):
         super().__init__()
+        self.menu_items = [{'viewclass': 'MDMenuItem',
+                            'text': 'Главная страница',
+                            'callback': self.callback_for_menu_items},
+                           {'viewclass': 'MDMenuItem',
+                            'text': 'Помощь',
+                            'callback': self.callback_for_menu_items},
+                           {'viewclass': 'MDMenuItem',
+                            'text': 'О нас',
+                            'callback': self.callback_for_menu_items}]
         return
+
+    def callback_for_menu_items(self, *args):
+        toast(args[0])
 
 
 class StartPage(BoxLayout):
@@ -19,16 +32,25 @@ class StartPage(BoxLayout):
         return
 
     def btn_new_press_function(self):
-        print("new")
+        self.MainLayout.clear()
+        self.MainLayout.page = NewNamePage()
+        self.MainLayout.add_widget(self.MainLayout.page)
         return
 
     def btn_old_press_function(self):
         self.MainLayout.clear()
-        self.MainLayout.add_widget(OldProjectsPage())
+        self.MainLayout.page = OldProjectsPage()
+        self.MainLayout.add_widget(self.MainLayout.page)
         return
 
 
-class OldProjectsPage(FloatLayout):
+class ListItem(OneLineListItem):
+    def __init__(self, text):
+        super().__init__(text=text)
+        return
+
+
+class OldProjectsPage(AnchorLayout):
     ListOfProjects = ["1 item", "2 item", '3 item']
     def __init__(self):
         super().__init__()
@@ -37,11 +59,16 @@ class OldProjectsPage(FloatLayout):
                           size_hint=(1., 1.))
         MDL = MDList()
         for name in self.ListOfProjects:
-            MDL.add_widget(OneLineListItem(text=name))
+            MDL.add_widget(ListItem(text=name))
         AL.add_widget(MDL)
         self.add_widget(AL)
         return
 
+
+class NewNamePage(AnchorLayout):
+    def __init__(self):
+        super().__init__()
+        return
 
 # главный слой
 class MainLayout(AnchorLayout):
